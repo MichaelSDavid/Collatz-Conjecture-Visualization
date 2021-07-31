5440efefdf694400ed4f9dac82233bfede7c914c
Michael David <michael_s_david@icloud.com>
2021-07-31 15:54:33 -0400

    Initial commit

---
+++ collatz.py
@@ -0,0 +1,33 @@
+# Import(s)
+import matplotlib.pyplot as plt
+
+# Var init and input collection
+n = int(input("Enter the initial value (n ∈ ℤ⁺): "))
+count = 0
+x = [0]
+y = []
+# Start sequence with n
+y.append(n)
+
+# Check conditions until the final number is 1
+while y[-1] != 1.0:
+	count += 1
+	x.append(count)
+	# Odd (not divisible by 2)
+	if y[-1] % 2 != 0:
+		y.append((3 * (y[-1])) + 1)
+	# Even (is divisible by 2)
+	elif y[-1] % 2 == 0:
+		y.append(y[-1] / 2)
+	print(y[-1], end="   ")
+
+# Zip coords
+for i in list(zip(x, y)):
+	plt.text(i[0], i[1], f"({i[0]} , {i[1]})")
+
+# Output to graph
+plt.xlabel("Iterations")
+plt.ylabel("Number (n)")
+plt.title(f"Collatz Conjecture for n = {n}")
+plt.plot(x, y, "bo--")
+plt.show()
